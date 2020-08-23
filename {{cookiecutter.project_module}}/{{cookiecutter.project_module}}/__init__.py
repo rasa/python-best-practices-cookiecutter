f401ca02768abcf758e4beab3d8126c99a2584ab
crashdummymch <mihaelchoffman@gmail.com>
2020-08-23 17:14:10 -0400

    updates

--- {{cookiecutter.project_module}}/{{cookiecutter.project_module}}/__init__.py
+++ {{cookiecutter.project_module}}/{{cookiecutter.project_module}}/__init__.py
@@ -6,10 +6,10 @@ def fib(number: int) -> int:
     if number < 0:
         raise ValueError("fib does not allow negative numbers")
     # First Fibonacci number is 0
-    elif n==1:
+    elif number == 1:
         return 0
     # Second Fibonacci number is 1
-    elif n==2:
+    elif number == 2:
         return 1
     else:
-        return fib(n-1)+fib(n-2)
+        return fib(number - 1) + fib(number - 2)
