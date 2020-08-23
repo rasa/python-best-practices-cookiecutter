5cfcbe62b47fa679a2e60642b9e2eca6eb15b4bd
crashdummymch <mihaelchoffman@gmail.com>
2020-08-23 17:08:54 -0400

    updates

--- {{cookiecutter.project_module}}/{{cookiecutter.project_module}}/__main__.py
+++ {{cookiecutter.project_module}}/{{cookiecutter.project_module}}/__main__.py
@@ -4,4 +4,4 @@ import {{cookiecutter.project_module}}
 
 if __name__ == "__main__":
     n = int(sys.argv[1])
-    print({{cokiecutter.project_module}}.fib(n))
+    print({{cookiecutter.project_module}}.fib(n))
