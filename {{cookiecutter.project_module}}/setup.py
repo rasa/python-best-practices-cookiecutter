75d9012cfada4f113b1a633b7843ad8b8e8c0d79
crashdummymch <mihaelchoffman@gmail.com>
2020-08-24 00:49:13 -0400

    update setup.py

--- {{cookiecutter.project_module}}/setup.py
+++ {{cookiecutter.project_module}}/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="{{cookiecutter.project_module}}",
     version="0.0.1",
     author_email="author@example.com",
@@ -11,7 +11,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     #url="https://github.com/pypa/sampleproject",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         #"License :: OSI Approved :: MIT License",
