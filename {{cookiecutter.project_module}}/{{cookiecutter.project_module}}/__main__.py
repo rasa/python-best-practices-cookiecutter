2abffb28a03f1763144134b522c2753e97a92de5
michael-c-hoffman <mihaelchoffman@gmail.com>
2021-07-20 22:36:29 -0400

    master-update

--- {{cookiecutter.project_module}}/{{cookiecutter.project_module}}/__main__.py
+++ {{cookiecutter.project_module}}/{{cookiecutter.project_module}}/__main__.py
@@ -1,7 +1,7 @@
 import sys
 
-from {{cookiecutter.project_module}}.fib import fib
+from {{cookiecutter.project_module}} import fib
 
 if __name__ == "__main__":
     n = int(sys.argv[1])
-    print(fib(n))
+    print(fib.fibx(n))
