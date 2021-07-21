9561c33a9eb878b8c8013d4a8c6c3c42ed25bc91
michael-c-hoffman <mihaelchoffman@gmail.com>
2021-07-20 20:22:07 -0400

    master-update for poetry

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,9 +1,9 @@
 import re
 import sys
 
-MODULE_REGEX = r"^[a-zA-Z][_a-zA-Z0-9]+$"
-module_name = "{{ cookiecutter.project_module }}"
+# MODULE_REGEX = r"^[a-zA-Z][_a-zA-Z0-9]+$"
+# module_name = "{{ cookiecutter.project_module }}"
 
-if not re.match(MODULE_REGEX, module_name):
-    print("ERROR: %s is not a valid Python module name!" % module_name)
-    sys.exit(1)
+# if not re.match(MODULE_REGEX, module_name):
+    # print("ERROR: %s is not a valid Python module name!" % module_name)
+    # sys.exit(1)
