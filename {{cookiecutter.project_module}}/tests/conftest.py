f59afba2e80c82c977ac7803d113203672d348dd
michael-c-hoffman <mihaelchoffman@gmail.com>
2021-07-22 22:52:08 -0400

    master-updated pylint and conftests

--- {{cookiecutter.project_module}}/tests/conftest.py
+++ {{cookiecutter.project_module}}/tests/conftest.py
@@ -1,28 +1,41 @@
+"""
+pytest fixtures and functions
+"""
 import importlib
 import pathlib
 import pytest
 
 
-def pytest_collection_modifyitems(config, items):
+def pytestCollectionModifyItems(config, items):
+    """
+    pytest add markers dynamically based on directory name
+    <directoryname>Test
+    """
     # python 3.4/3.5 compat: rootdir = pathlib.Path(str(config.rootdir))
     rootdir = pathlib.Path(config.rootdir)
     for item in items:
-        rel_path = pathlib.Path(item.fspath).relative_to(rootdir)
-        for part in rel_path.parts:
+        relPath = pathlib.Path(item.fspath).relative_to(rootdir)
+        for part in relPath.parts:
             if part.endswith("Tests"):
                 mark = getattr(pytest.mark, part)
                 item.add_marker(mark)
 
 
-def pytest_generate_tests(metafunc):
+def pytestGenerateTests(metafunc):
+    """
+    If fixture begins with data_ add a parametrize fixture
+    """
     for fixture in metafunc.fixturenames:
         if "data_" in fixture:
-            metafunc.parametrize(fixture, load_tests(fixture))
+            metafunc.parametrize(fixture, loadTests(fixture))
 
 
-def load_tests(name):
+def loadTests(name):
+    """
+    loads test data dynamically utilizing data_<name>.py in tests/data directory
+    """
     # Load module which contains test data
-    tests_module = importlib.import_module("." + name, "data")
+    testsModule = importlib.import_module("." + name, "data")
     # Tests are to be found in the variable `tests` of the module
-    for test in tests_module.tests:
+    for test in testsModule.tests:
         yield test
