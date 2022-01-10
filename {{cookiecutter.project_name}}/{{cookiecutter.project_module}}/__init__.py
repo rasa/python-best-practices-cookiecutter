eec18eba1664569ce8d538fbf9e1ec41c1baaee0
michael-c-hoffman <mihaelchoffman@gmail.com>
2022-01-09 23:16:38 -0500

    master::added logger

--- {{cookiecutter.project_name}}/{{cookiecutter.project_module}}/__init__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_module}}/__init__.py
@@ -1,4 +1,9 @@
 """
 {{cookiecutter.project_name}}
+{{cookiecutter.project_module}}
 {{cookiecutter.project_description}}
 """
+
+import logging
+
+logging.getLogger("{{cookiecutter.project_module}}").addHandler(logging.NullHandler())
