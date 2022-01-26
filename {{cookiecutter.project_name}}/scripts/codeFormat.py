e19c1e4e26e718d9e99f2b0c6e878f6fa64f76ed
michael-c-hoffman <mihaelchoffman@gmail.com>
2022-01-25 20:55:47 -0500

    master::updates

--- {{cookiecutter.project_name}}/scripts/codeFormat.py
+++ {{cookiecutter.project_name}}/scripts/codeFormat.py
@@ -4,7 +4,6 @@ import pytest
 def mypyTests(args=None):
     pytest.main(["--mypy", "-m", "mypy"])
 
-
 def isortTests():
     pytest.main(["--isort", "-m", "isort"])
 
@@ -14,10 +13,11 @@ def isortFix():
 def blackTests():
     pytest.main(["--black", "-m", "black"])
 
-
 def blackFix():
     os.system("black {{cookiecutter.project_module}} tests scripts")
 
+def pylintTests():
+    pytest.main(["--pylint", "-m", "pylint"])
 
 def flake8Tests():
     pytest.main(["--flake8", "-m", "flake8"])
@@ -26,7 +26,7 @@ def pytestCovTerm():
     pytest.main(["--cov-report=term-missing", "--cov={{cookiecutter.project_module}}", "tests/"])
 
 def pytestTestReports():
-    pytest.main(["--isort", "--black", "--mypy", "--pylint", "--html=reports/codeTests/index.html"])
+    pytest.main(["--isort", "--black", "--mypy", "--pylint", "--capture=sys", "--html=reports/codeTests/index.html"])
 
 def pytestCovReports():
     pytest.main(["--cov", "--cov-fail-under=50", "--cov-report=html:reports/codeCov/"])
--- {{cookiecutter.project_name}}/tests/unitTests/{{cookiecutter.project_name}}Tests.py
+++ {{cookiecutter.project_name}}/tests/unitTests/{{cookiecutter.project_name}}Tests.py
@@ -1,5 +1,13 @@
+import logging
+
 from {{cookiecutter.project_module}}.fib import fib
 
+logger = logging.getLogger(__name__)
+streamHandler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+streamHandler.setFormatter(formatter)
+logger.addHandler(streamHandler)
+logger.setLevel(logging.INFO)
 
 def fibTest(data_fib) -> None:
     assert fib(data_fib[0]) == data_fib[1]
